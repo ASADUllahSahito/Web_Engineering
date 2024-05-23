c86fb742daa286dd5089fb80b51b8acb6f9d9844
Asad Ullah <98896686+ASADUllahSahito@users.noreply.github.com>
2024-05-23 22:08:44 +0500

    Add files via upload

---
+++ React/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   
+    <div>
+    <Square side={100} />
+    <Rectangle width={200} height={100} />
+    <Circle radius={50} />
+  </div>
+
+
+
+  );
+}
+
+export default App;
+

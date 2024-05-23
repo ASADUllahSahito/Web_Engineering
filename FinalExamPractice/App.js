b90fe74ea207a7dd8e0f52e10ca8399bf63d5d53
Asad Ullah <98896686+ASADUllahSahito@users.noreply.github.com>
2024-05-23 22:14:02 +0500

    Add files via upload

---
+++ FinalExamPractice/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  function shoot() {
+
+    
+    const ply = ()=>{
+      alert("great sht")
+    }
+    ply();
+   }
+  return (
+   <>
+   <button onClick = {shoot}>Play the game</button>
+   </>
+   
+  );
+  
+}
+
+export default App;
